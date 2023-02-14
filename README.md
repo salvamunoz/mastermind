da77577e3703bfe45d994df3010c805a034820f7
Salva Munoz <salva17y2@gmail.com>
2023-02-14 21:11:44 +0100

    adding README.md and requirements.txt, latest improvements to main.py

---
+++ README.md
@@ -0,0 +1,61 @@
+# Rules of the game
+The goal of the game is to guess the randomly generated code consisting of 4 colors from the following: red (R), green (G), blue (B), and yellow (Y).
+
+Each time a guess is sent, the API will respond with the number of black and white pegs obtained in that guess.
+
+**Black peg**: obtained when a color is correctly guessed in the correct position
+
+**White peg**: obtained when a color is correctly guessed in the incorrect position
+The game ends when the code is correctly guessed or after a determined number of attempts.
+# Mastermind API
+This API provides a RESTful interface to play the game Mastermind. It consists of three endpoints:
+
+## Endpoints
+### POST /game
+Creates a new game of Mastermind. The secret code is generated randomly, and consists of four characters, each one being one of 'R', 'G', 'B', or 'Y'. The secret code is stored in the database and is compared against the user's guesses.
+
+### GET /game/{game_id}
+Retrieves the current state of the game. The game is identified by a unique ID, which is passed as a parameter in the URL. The response contains the following information:
+
+- game_id: the unique identifier for the game.
+- secret_code: the secret code for the game, represented as a string of four characters.
+- guesses: a list of the user's guesses, each one represented as a string of four characters.
+- feedback: a list of feedback for each guess, represented as a string of four characters. Each character represents the feedback for one of the four positions in the guess. A 'B' means that the color is correct and in the right position, while a 'W' means that the color is correct but in the wrong position.
+### POST /game/{game_id}/guess
+Makes a new guess for an ongoing game. The guess is passed in the request body as a JSON object, with the following format:
+
+```
+  "guess": "RRBB"
+```
+The response contains the same information as the GET /game/{game_id} endpoint, updated with the new guess and its feedback.
+### DELETE /game/{game_id}/delete
+Delete an existing game identified by a unique ID, which is passed as a parameter in the URL.
+
+# How to run a FASTAPI code
+To run a FASTAPI code, you will need to have the following prerequisites installed:
+
+- Python 3.6 or higher
+- FastAPI and its dependencies
+
+Once you have the prerequisites installed, follow the steps below to run your FASTAPI code:
+
+1. Clone or download the repository containing the FASTAPI code.
+2. Change to the directory where the code is located in your local machine.
+3. Create a virtual environment (optional but recommended) to keep the dependencies required for this project separate from your global Python environment.
+````commandline
+python3 -m venv venv
+source venv/bin/activate
+````
+4. Install the dependencies required for this project using the following command:
+````commandline
+pip install -r requirements.txt
+````
+5. Run the FASTAPI code using the following command:
+````commandline
+uvicorn main:app --reload
+````
+6. The code should start running and you should see output similar to the following:
+````commandline
+INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
+````
+7. Open your web browser and navigate to http://127.0.0.1:8000 to access the FASTAPI endpoint
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel
 from pymongo import ReturnDocument
 from v1.mastermindv1 import get_next_nr_game, check_guess, NR_CHAR
 from core.db_init import collection
-from starlette.responses import JSONResponse
 import random
 import logging
 
 app = FastAPI()
-log = logging.getLogger("my-api")
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 
 class Guess(BaseModel):
@@ -27,46 +27,43 @@ def get_root_page():
     return {"Hello": "World"}
 
 
-@app.post("/game")
+@app.post("/game", status_code=201)
 async def create_game():
+    log.info("")
     # k: number of elements picked randomly
     secret_code = "".join(random.choices(["R", "G", "B", "Y"], k=NR_CHAR))
     nr_game_id = get_next_nr_game()
+    log.info(f"[create_game] Creating game nr {nr_game_id}")
     collection.insert_one({"secret_code": secret_code, "game_id": nr_game_id})
-    return JSONResponse(content={"msg": f"Game created successfully. Game ID: {nr_game_id}, secret_code {secret_code}"},
-                        status_code=201)
+    return {"msg": f"Game created successfully. Game ID: {nr_game_id}, secret_code {secret_code}"}
 
 
-@app.get("/game/{game_id}")
+@app.get("/game/{game_id}", status_code=200)
 async def get_game(game_id: int):
+    log.info(f"[get_game] game id: {game_id}")
     game_info = collection.find_one({"game_id": game_id}, {"_id": False, "secret_code": False})
     guesses = game_info.get("guesses", [])
     b_pegs = game_info.get("b_pegs", 0)
     w_pegs = game_info.get("w_pegs", 0)
-    return JSONResponse(content={"id": game_id,
-                                 "Previous guesses": guesses,
-                                 "black pegs": b_pegs,
-                                 "white pegs": w_pegs},
-                        status_code=200)
+    return {"id": game_id, "Previous guesses": guesses, "black pegs": b_pegs, "white pegs": w_pegs}
 
 
-@app.post("/game/{game_id}/guess")
+@app.post("/game/{game_id}/guess", status_code=201)
 async def make_guess(game_id: int, guess: Guess):
     guess.guess = guess.guess.upper()
-    log.info(f"game id: {game_id}, guess: {guess.guess}")
+    log.info(f"[make_guess] game id: {game_id}, guess: {guess.guess}")
     game_info = collection.find_one_and_update({"game_id": game_id},
                                                {"$push": {"guesses": guess.guess}},
                                                projection={"_id": False, "secret_code": True},
                                                return_document=ReturnDocument.AFTER)
     if game_info["secret_code"] == guess.guess:
-        return JSONResponse(content={"id": game_id, "secret_code": guess.guess, "msg": "success"}, status_code=201)
+        return {"id": game_id, "secret_code": guess.guess, "msg": "success"}
     else:
         b_pegs, w_pegs = check_guess(guess.guess, game_info["secret_code"])
-        return JSONResponse(content={"id": game_id, "msg": "keep trying", "black pegs": b_pegs, "white pegs": w_pegs},
-                            status_code=201)
+        return {"id": game_id, "msg": "keep trying", "black pegs": b_pegs, "white pegs": w_pegs}
 
 
-@app.delete("/game/{game_id}")
+@app.delete("/game/{game_id}/delete", status_code=204)
 async def delete_game(game_id: int):
-    collection.delete_one({"game_id": game_id})
-    return JSONResponse(content={"msg": "deleted"}, status_code=204)
+    log.info(f"[delete_game] game id: {game_id}")
+    _ = collection.find_one_and_delete({"game_id": game_id}, projection={"_id": False})
